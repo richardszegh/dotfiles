7b26f4ab64d9bc1250d4d6bfd8498cdcb4be22e7
Richárd Szegh <rich@richardszegh.com>
2023-02-04 13:21:57 +0100

    Update dotfile paths

--- macos.sh
+++ macos.sh
@@ -5,7 +5,7 @@ brew install autojump bat ffmpeg git gnupg jq neofetch nmap ranger ripgrep wifi-
 brew install --cask kitty macvim
 
 # alias dotfiles
-ln -s "${HOME}/Workspace/public/dotfiles/.zshrc" "${HOME}/.zshrc"
-ln -s "${HOME}/Workspace/public/dotfiles/.vimrc" "${HOME}/.vimrc"
-ln -s "${HOME}/Workspace/public/dotfiles/.gitconfig" "${HOME}/.gitconfig"
-ln -s "${HOME}/Workspace/public/dotfiles/.ssh/config" "${HOME}/.ssh/config"
+ln -s "${HOME}/Workspace/dotfiles/.zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/Workspace/dotfiles/.vimrc" "${HOME}/.vimrc"
+ln -s "${HOME}/Workspace/dotfiles/.gitconfig" "${HOME}/.gitconfig"
+ln -s "${HOME}/Workspace/dotfiles/.ssh/config" "${HOME}/.ssh/config"
