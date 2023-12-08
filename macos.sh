72422a9f79f95eb7d329b102da05168b348d12ee
Richard Szegh <rich@richardszegh.com>
2023-12-08 11:57:02 +0100

    add `brew install tldr`

--- macos.sh
+++ macos.sh
@@ -30,7 +30,7 @@ fi
 # (2/9) install brew packages
 echo "[macos.sh](info) installing brew packages..."
 brew install autojump bat ffmpeg fd git gnupg httpie jq nmap ranger ripgrep \
-  rsync wifi-password yt-dlp
+  rsync tldr wifi-password yt-dlp
 echo "[macos.sh](info) finished installing brew packages"
 
 # (3/9) install brew casks
