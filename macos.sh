9b8aee688f8a630f2a18067563e1fb883d1c7a90
Richard Szegh <rich@richardszegh.com>
2024-04-22 10:44:34 +0200

    brave->chrome

--- macos.sh
+++ macos.sh
@@ -35,7 +35,7 @@ echo "[macos.sh](info) finished installing brew packages"
 
 # (3/9) install brew casks
 echo "[macos.sh](info) installing brew casks..."
-brew install --cask brave-browser firefox 1password foobar2000 spotify pocket-casts sunsama fantastical thunderbird slack loom zoom google-drive \
+brew install --cask google-chrome firefox 1password foobar2000 spotify pocket-casts sunsama fantastical thunderbird slack loom zoom google-drive \
   figma exifcleaner audacity handbrake losslesscut \
   kitty visual-studio-code orbstack insomnia \
   obsidian \
