2bf16460ec418c4ccbeb3d57c4274f7253295582
Richard Szegh <rich@richardszegh.com>
2024-05-12 14:23:10 +0200

    update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(after! evil
+	(evil-ex-define-cmd "wq" 'doom/save-and-kill-buffer)
+	(evil-ex-define-cmd "q" 'kill-this-buffer)
+)
