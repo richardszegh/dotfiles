4f53fcb047114dc5157075ab3b410a02a1c89074
Richard Szegh <rich@richardszegh.com>
2024-09-20 19:28:22 +0200

    remove arc

--- macos.sh
+++ macos.sh
@@ -35,7 +35,7 @@ echo "[macos.sh](info) finished installing brew packages"
 
 # (3/9) install brew casks
 echo "[macos.sh](info) installing brew casks..."
-brew install --cask arc google-chrome firefox 1password pocket-casts \
+brew install --cask firefox google-chrome 1password pocket-casts \
   sunsama fantastical slack loom zoom google-drive \
   figma exifcleaner audacity handbrake losslesscut \
   kitty visual-studio-code webstorm orbstack insomnia wireshark fork linear-linear \
